fdae695a2642b7e24d99d9b8d54dbe1dd0f3f1cd
Ashish-M-Sharma <a.m.sharma2501@gmail.com>
2025-05-14 20:34:50 +0530

    "added .env to github"

--- app/utils/stripHtmlTags.ts
+++ app/utils/stripHtmlTags.ts
@@ -1,8 +1,8 @@
-// app/utils/sanitize.ts
+
 import DOMPurify from "dompurify";
 
 export const stripHtmlTags = (htmlContent: string): string => {
-  // Sanitize the HTML using DOMPurify
-  const sanitizedContent = DOMPurify.sanitize(htmlContent, { ALLOWED_TAGS: [] }); // This removes all tags.
+ 
+  const sanitizedContent = DOMPurify.sanitize(htmlContent, { ALLOWED_TAGS: [] }); 
   return sanitizedContent;
 };
